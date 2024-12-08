fba2ac1293f7ed8aa5793d4f31a36ec1ac336a43
pwalig <pwalig03@gmail.com>
2024-12-08 22:28:27 +0100

    Handle client messages in player class

--- src/rts/player.cpp
+++ src/rts/player.cpp
@@ -1,9 +1,29 @@
 #include "player.h"
+
 #include <cassert>
+
+#include "../msg/name.h"
 
 std::unordered_map<std::string, rts::player*> rts::player::playersByName;
 
-rts::player::player(client* client_) : _client(client_) {}
+rts::player::player(client* client_) : _client(client_), mh(client_) {
+    mh.onNewMessage = std::bind(&rts::player::handleNewMessage, this, std::placeholders::_1);
+}
+
+void rts::player::handleNewMessage(const message::base* msg) {
+    if (msg->typ == message::type::name){
+        const message::name* cmsg = dynamic_cast<const message::name*>(msg);
+        assert(cmsg);
+        if (cmsg->_name != _name && nameTaken(cmsg->_name)) { // other player has that name
+            _client->sendToClient({'n'});
+        }
+        else if (_name == "") name(cmsg->_name);
+        else reName(cmsg->_name);
+    }
+    else if (msg->typ == message::type::invalid) {
+        _client->sendToClient({'i'});
+    }
+}
 
 void rts::player::name(const std::string& name) {
     assert(playersByName.find(name) == playersByName.end());
@@ -16,4 +36,8 @@ void rts::player::reName(const std::string& name) {
     playersByName.erase(_name);
     _name = name;
     playersByName.insert({_name, this});
+}
+
+bool rts::player::nameTaken(const std::string& name) {
+    return (playersByName.find(name) != playersByName.end());
 }
--- src/rts/player.h
+++ src/rts/player.h
@@ -4,17 +4,22 @@
 #include <unordered_map>
 
 #include "../net/client.h"
+#include "../msg/handler.h"
 
 namespace rts {
     class player {
     private:
         client* _client;
+        message::handler mh;
         std::string _name = "";
+
+        void handleNewMessage(const message::base* msg);
 
         static std::unordered_map<std::string, player*> playersByName;
     public:
         player(client* client_);
         void name(const std::string& name);
         void reName(const std::string& name);
+        static bool nameTaken(const std::string& name);
     };
 }
