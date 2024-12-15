b3426feef40f2879e10edbbedf8afaa6a9d188b8
pwalig <pwalig03@gmail.com>
2024-12-15 18:55:31 +0100

    Prevent copying units

--- src/rts/unit.hpp
+++ src/rts/unit.hpp
@@ -13,6 +13,12 @@ namespace rts {
         bool movedThisRound = false;
 
         unit(player* owner_, field* field_, unsigned int id_);
+
+        unit(const unit& other) = delete;
+        unit(unit&& other) = delete;
+        unit& operator=(const unit& other) = delete;
+        unit& operator=(unit&& other) = delete;
+        
         void mine();
         void move(field* field_);
         void attack(unit* target);
