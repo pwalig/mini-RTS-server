3d8a9714c15ec920e0fa351ada06e0c0d5b14011
pwalig <pwalig03@gmail.com>
2025-02-01 23:02:31 +0100

    Fix build.zig

--- build.zig
+++ build.zig
@@ -18,6 +18,17 @@ pub fn build(b: *std.Build) void {
         }),
     });
     exe.addCSourceFile(.{ .file = b.path("src/main.cpp") });
+    exe.addCSourceFile(.{ .file = b.path("src/rts/game.cpp") });
+    exe.addCSourceFile(.{ .file = b.path("src/rts/board.cpp") });
+    exe.addCSourceFile(.{ .file = b.path("src/rts/player.cpp") });
+    exe.addCSourceFile(.{ .file = b.path("src/rts/field.cpp") });
+    exe.addCSourceFile(.{ .file = b.path("src/rts/unit.cpp") });
+    exe.addCSourceFile(.{ .file = b.path("src/net/server.cpp") });
+    exe.addCSourceFile(.{ .file = b.path("src/net/client.cpp") });
+    exe.addCSourceFile(.{ .file = b.path("src/msg/addressUnit.cpp") });
+    exe.addCSourceFile(.{ .file = b.path("src/msg/handler.cpp") });
+    exe.addCSourceFile(.{ .file = b.path("src/msg/stringBuffer.cpp") });
+    exe.addCSourceFile(.{ .file = b.path("src/msg/unitCommands.cpp") });
     exe.addIncludePath(b.path("src/"));
     exe.linkLibCpp();
     exe.linkLibC();
